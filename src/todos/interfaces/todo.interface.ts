6a9a2430eca59aaed9df9183c269661bd4bc573b
Khoa Nguyen tien <ddtrong@tma.com.vn>
2024-07-03 22:01:40 +0700

    Comment that Todo Interface is currently not used

--- src/todos/interfaces/todo.interface.ts
+++ src/todos/interfaces/todo.interface.ts
@@ -1,3 +1,4 @@
+// Now we use Entity Todo instead of Interface Todo in TodosController and TodosService, so this Interface is currently not used anywhere
 export interface Todo {
   name: string;
   isDone: boolean;
