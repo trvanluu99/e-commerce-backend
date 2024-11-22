37ca7dc012ee2481b960a9d0eee2e76593986e41
Louis Tran <trvanluu99@gmail.com>
2024-11-22 17:46:06 +0700

    Increase the expired time of the token to 1 day

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { AuthGuard } from './auth.guard';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '600s' },
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
